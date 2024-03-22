796fa15c8e78b8f154174a66ca6f1c04c1a38d06
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-03-22 08:18:03 -0300

    Criando nossa página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import heroImg from "@/assets/hero.svg";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-[calc(100vh - 80px)]">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
+      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">Atendimentos, clientes</h1>
+
+      <Image
+        src={heroImg}
+        alt="Imagem principal DevControle"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
+    </main>
   );
 }
