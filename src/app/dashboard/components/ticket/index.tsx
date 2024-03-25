c88ab6db59abd4dd7f4b2831146a4d501d1624da
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-03-25 12:46:00 -0300

    Criando tabela chamados

---
+++ src/app/dashboard/components/ticket/index.tsx
@@ -0,0 +1,23 @@
+import { FiTrash2, FiFile } from "react-icons/fi";
+
+export function TicketItem() {
+  return (
+    <>
+      <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200">
+        <td className="text-left pl-1">Rafael</td>
+        <td className="text-left hidden sm:table-cell">25/03/2024</td>
+        <td className="text-left">
+          <span className="bg-green-500 px-2 py-1 rounded">ABERTO</span>
+        </td>
+        <td className="text-left">
+          <button className="mr-2">
+            <FiTrash2 size={24} color="#ef4444" />
+          </button>
+          <button>
+            <FiFile size={24} color="#3b82f6" />
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+}
