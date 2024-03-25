814f089a574667a2e855c5d1200a2c575ff1863e
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-03-25 12:59:40 -0300

    Criando página detalhes

---
+++ src/app/dashboard/customer/new/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/container";
+
+export default function NewCustomer() {
+  return (
+    <Container>
+      <h1>Página de cadastro do cliente</h1>
+    </Container>
+  );
+}
