1e2f42556065f1b58002a4ed2253205615a29e36
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-03-26 10:39:21 -0300

    Cadastrando cliente

--- src/app/dashboard/customer/components/form/index.tsx
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -4,6 +4,8 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/input";
+import { api } from "@/lib/api";
+import { useRouter } from "next/navigation";
 
 const schema = z.object({
   name: z.string().min(1, "O campo nome é obrigatório"),
@@ -28,7 +30,7 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>;
 
-export function NewCustomerForm() {
+export function NewCustomerForm({ userId }: { userId: string }) {
   const {
     register,
     handleSubmit,
@@ -37,7 +39,19 @@ export function NewCustomerForm() {
     resolver: zodResolver(schema),
   });
 
-  function handleRegisterCustomer(data: FormData) {}
+  const router = useRouter();
+
+  async function handleRegisterCustomer(data: FormData) {
+    const response = await api.post("/api/customer", {
+      name: data.name,
+      phone: data.phone,
+      email: data.email,
+      address: data.address,
+      userId: userId,
+    });
+
+    router.replace("/dashboard/customer");
+  }
 
   return (
     <form
