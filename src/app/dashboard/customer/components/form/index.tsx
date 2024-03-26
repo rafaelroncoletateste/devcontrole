daa32f0553e1c1bdea227b3954e3d60b543506bd
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-03-26 09:50:56 -0300

    Criando input personalizado

--- src/app/dashboard/customer/components/form/index.tsx
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -3,6 +3,7 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/input";
 
 const schema = z.object({
   name: z.string().min(1, "O campo nome é obrigatório"),
@@ -28,14 +29,77 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 export function NewCustomerForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
+  function handleRegisterCustomer(data: FormData) {}
+
   return (
-    <form action="#">
-      <label htmlFor="#">Nome completo</label>
-      <input type="text" placeholder="Digite o nome completo" />
+    <form
+      action="#"
+      onSubmit={handleSubmit(handleRegisterCustomer)}
+      className="flex flex-col mt-6"
+    >
+      <label htmlFor="#" className="mb-1 text-lg font-medium">
+        Nome completo
+      </label>
+      <Input
+        type="text"
+        name="name"
+        placeholder="Digite o nome completo"
+        error={errors.name?.message}
+        register={register}
+      />
+
+      <section className="flex gap-2 my-2 flex-col sm:flex-row">
+        <div className="flex-1">
+          <label htmlFor="#" className="mb-1 text-lg font-medium">
+            Telefone
+          </label>
+          <Input
+            type="number"
+            name="phone"
+            placeholder="Digite o seu celular"
+            error={errors.phone?.message}
+            register={register}
+          />
+        </div>
+        <div className="flex-1">
+          <label htmlFor="#" className="mb-1 text-lg font-medium">
+            Email
+          </label>
+          <Input
+            type="email"
+            name="email"
+            placeholder="Digite o email"
+            error={errors.email?.message}
+            register={register}
+          />
+        </div>
+      </section>
+
+      <label htmlFor="#" className="mb-1 text-lg font-medium">
+        Endereço
+      </label>
+      <Input
+        type="text"
+        name="address"
+        placeholder="Digite o seu celular"
+        error={errors.address?.message}
+        register={register}
+      />
+
+      <button
+        type="submit"
+        className="bg-blue-500 my-4 px-2 h-11 rounded text-white font-bold"
+      >
+        Cadastrar
+      </button>
     </form>
   );
 }
