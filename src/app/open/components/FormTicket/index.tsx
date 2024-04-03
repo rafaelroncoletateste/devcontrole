512b7776b0543ced2555b5e51db22db1f902fb3e
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-04-03 09:59:43 -0300

    Cadastrando chamado aberto pela página open

--- src/app/open/components/FormTicket/index.tsx
+++ src/app/open/components/FormTicket/index.tsx
@@ -4,6 +4,8 @@ import { Input } from "@/components/input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { api } from "@/lib/api";
+import { CustomerDataInfo } from "../../page";
 
 const schema = z.object({
   name: z.string().min(1, "O nome do chamado é obrigatório"),
@@ -12,7 +14,11 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>;
 
-export function FormTicket() {
+interface FormTicketProps {
+  customer: CustomerDataInfo;
+}
+
+export function FormTicket({ customer }: FormTicketProps) {
   const {
     register,
     handleSubmit,
@@ -22,8 +28,19 @@ export function FormTicket() {
     resolver: zodResolver(schema),
   });
 
+  async function handleRegisterTicket(data: FormData) {
+    const response = await api.post("/api/tickets", {
+      name: data.name,
+      description: data.description,
+      customerId: customer.id,
+    });
+  }
+
   return (
-    <form className="bg-slate-200 mt-6 px-4 py-3 rounded border-2">
+    <form
+      onSubmit={handleSubmit(handleRegisterTicket)}
+      className="bg-slate-200 mt-6 px-4 py-3 rounded border-2"
+    >
       <label className="mb-1 font-medium text-lg">Nome do Chamado</label>
       <Input
         register={register}
@@ -37,11 +54,11 @@ export function FormTicket() {
       <textarea
         placeholder="Descreva o seu problema"
         id="description"
-        className="w-full border-2 rounded-md h-24 resize-none mb-2 px-2"
+        className="w-full border-2 rounded-md h-24 resize-none px-2"
         {...register("description")}
       ></textarea>
       {errors.description?.message && (
-        <p className="text-red-500 my-1">{errors.description?.message}</p>
+        <p className="text-red-500 mt-1 mb-4">{errors.description?.message}</p>
       )}
 
       <button
--- src/app/open/page.tsx
+++ src/app/open/page.tsx
@@ -19,7 +19,7 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>;
 
-interface CustomerDataInfo {
+export interface CustomerDataInfo {
   id: string;
   name: string;
 }
@@ -107,7 +107,7 @@ export default function OpenTicket() {
           </form>
         )}
 
-        {customer !== null && <FormTicket />}
+        {customer !== null && <FormTicket customer={customer} />}
       </main>
     </div>
   );
